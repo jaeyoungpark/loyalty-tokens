827618a4517c9b660e1b15676271630b654af506
Jae Young Park <jae.park@jaepark-ltm4.internal.salesforce.com>
2022-09-01 21:31:00 -0700

    nit fix

--- javascript/golfswings/pages/[invitationCode].tsx
+++ javascript/golfswings/pages/[invitationCode].tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
   const query = router.query;
   const invitationCode = query.invitationCode?.toString();
   var placeholderCode = invitationCode
-  if (!validCodes.includes(invitationCode?.toLowerCase())) {
+  if (!validCodes.includes(invitationCode?)) {
     placeholderCode = "Invitation Code"
   }
 
