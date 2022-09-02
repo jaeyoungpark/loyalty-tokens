45a9ea0826d605aad6764a373248626f9599f9f9
jaeyoungpark <owlrocks@berkeley.edu>
2022-09-01 19:46:35 -0700

    Delete typescript directory

--- typescript/dynamic-allowlist-edition/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["gateway.ipfscdn.io"],
-  },
-};
-
-module.exports = nextConfig;
--- typescript/free-first-mint/utils/thirdweb.ts
+++
@@ -1,8 +0,0 @@
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-
-const sdk = ThirdwebSDK.fromPrivateKey(
-  process.env.PRIVATE_KEY as string,
-  "mumbai"
-);
-
-export default sdk;
